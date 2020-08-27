31a3dc04569d40d86187dfabeec9c645bda9a03c
Shraddhesh Singh <sids@protonmail.com>
2020-08-27 13:37:06 +0530

    Migrated from 2.2 to 3.1

--- Mmd.Ui/Startup.cs
+++ Mmd.Ui/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace mmd_plus
 {
@@ -36,13 +37,19 @@ namespace mmd_plus
             string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION") ?? Configuration.GetConnectionString("CodeCompDatabase");
 
             services.AddDbContext<DataContext>(options =>
-                    options.UseSqlServer(connectionString)) ;   
+            {
+                options.UseSqlServer(connectionString);
+            }) ;
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options => 
+            {
+                options.EnableEndpointRouting = false;
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
