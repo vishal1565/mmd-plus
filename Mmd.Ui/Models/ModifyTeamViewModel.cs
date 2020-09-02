7444314ad8c4bc43973a40cf40969f82fcc60db0
Shraddhesh Singh <sids@protonmail.com>
2020-09-02 15:35:31 +0530

    added regular expressions

--- Mmd.Ui/Models/ModifyTeamViewModel.cs
+++ Mmd.Ui/Models/ModifyTeamViewModel.cs
@@ -8,7 +8,8 @@ namespace mmd_plus.Models
 {
     public class ModifyTeamViewModel
     {
-        [Required(ErrorMessage = "TeamId Required")]
+        [Required(ErrorMessage = "TeamId Required"), StringLength(20, ErrorMessage = "Atleast 5 and max 20 characters allowed", MinimumLength = 5)]
+        [RegularExpression("^(?=[a-z0-9._]{5,20}$)(?=.*[a-z]+.*)(?!.*[_.]{2})[^_.].*[^_.]$", ErrorMessage = "Only a-z 0-9 . _ characters allowed")]
         public string TeamId { get; set; }
         [Required(ErrorMessage = "Token Required"), StringLength(32, ErrorMessage = "Incorrect Length", MinimumLength = 32)]
         public string SecretToken { get; set; }
--- Mmd.Ui/Models/RegisterViewModel.cs
+++ Mmd.Ui/Models/RegisterViewModel.cs
@@ -11,7 +11,8 @@ namespace mmd_plus.Models
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "TeamId Required")]
+        [Required(ErrorMessage = "TeamId Required"), StringLength(20, ErrorMessage = "Atleast 5 and max 20 characters allowed", MinimumLength = 5)]
+        [RegularExpression("^(?=[a-z0-9._]{5,20}$)(?=.*[a-z]+.*)(?!.*[_.]{2})[^_.].*[^_.]$", ErrorMessage = "Only a-z 0-9 . _ characters allowed")]
         public string TeamId { get; set; }
         public List<TeamMember> TeamMembers { get; set; }
 
@@ -23,7 +24,7 @@ namespace mmd_plus.Models
 
     public class TeamMember
     {
-        [Required(ErrorMessage = "Field Required")]
+        [Required(ErrorMessage = "Field Required"), StringLength(60, ErrorMessage = "Character Limit Exceeded")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string EmailId { get; set; }
     }
