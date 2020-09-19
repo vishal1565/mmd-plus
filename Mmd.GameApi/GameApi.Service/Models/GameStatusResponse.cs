36a3d246c0dc4c7a227181f610c4d4285a087af5
Shraddhesh Singh <sids@protonmail.com>
2020-09-19 13:38:37 +0530

    added values

--- Mmd.GameApi/GameApi.Service/Models/GameStatusResponse.cs
+++ Mmd.GameApi/GameApi.Service/Models/GameStatusResponse.cs
@@ -6,10 +6,23 @@ namespace GameApi.Service.Models
 {
     public class GameStatusResponse
     {
+        public Error Err { get; set; }
+        public Data Data { get; set; }
+    }
+
+    public class Error
+    {
+        public string Message { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Data
+    {
         public Guid GameId { get; set; }
         public Guid RoundId { get; set; }
         public PhaseType RoundPhase { get; set; }
         public int RoundNumber { get; set; }
+        public int SecretLength { get; set; }
         public List<TeamData> Participants { get; set; }
 
     }
--- Mmd.GameApi/GameApi.Service/Models/TeamData.cs
+++ Mmd.GameApi/GameApi.Service/Models/TeamData.cs
@@ -3,6 +3,8 @@ namespace GameApi.Service.Models
     public class TeamData
     {
         public string TeamId { get; set; }
-        public long Score { get; set; }
+        public long CurrentScore { get; set; }
+        public long TotalScore { get; set; }
+        public bool IsAlive { get; set; }
     }
 }
