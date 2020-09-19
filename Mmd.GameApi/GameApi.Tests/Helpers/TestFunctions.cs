67df375928c892f224c5d28a6452ca6ca5a7300e
Shraddhesh Singh <sids@protonmail.com>
2020-09-19 18:21:24 +0530

    added async testing support

---
+++ Mmd.GameApi/GameApi.Tests/Helpers/TestFunctions.cs
@@ -0,0 +1,28 @@
+using System.Threading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameApi.Tests.Helpers.TestClasses;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace GameApi.Tests.Helpers
+{
+
+    public static class TestFunctions
+    {
+
+        // Return a DbSet of the specified generic type with support for async operations
+        public static Mock<DbSet<T>> GetDbSet<T>(IQueryable<T> TestData) where T : class
+        {
+            var MockSet = new Mock<DbSet<T>>();
+            MockSet.As<IAsyncEnumerable<T>>().Setup(x => x.GetAsyncEnumerator(default(CancellationToken))).Returns(new TestAsyncEnumerator<T>(TestData.GetEnumerator()));
+            MockSet.As<IQueryable<T>>().Setup(x => x.Provider).Returns(new TestAsyncQueryProvider<T>(TestData.Provider));
+            MockSet.As<IQueryable<T>>().Setup(x => x.Expression).Returns(TestData.Expression);
+            MockSet.As<IQueryable<T>>().Setup(x => x.ElementType).Returns(TestData.ElementType);
+            MockSet.As<IQueryable<T>>().Setup(x => x.GetEnumerator()).Returns(TestData.GetEnumerator());
+            return MockSet;
+        }
+
+    }
+}
