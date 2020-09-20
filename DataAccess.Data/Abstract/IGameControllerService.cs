a5b9c3f55ff7b4320dacb2f61fd6dcc9d7885191
meghraj jagtap <itsmemeghraj@gmail.com>
2020-09-20 16:56:24 +0530

    added gameControllerService

--- DataAccess.Data/Abstract/IGameControllerService.cs
+++ DataAccess.Data/Abstract/IGameControllerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess.Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,11 @@ namespace DataAccess.Data.Abstract
 {
     public interface IGameControllerService
     {
+
+        void AddNewGame(GameModel currentGame);
+        void AddNewRound(RoundConfig round, GameModel game);
+        void AddNewPhase(RoundConfig round, PhaseType phase, DateTime timeStamp, Guid gameId);
+        void EndGame(GameModel currentGame);
+        void CollectPenalty(RoundConfig round);
     }
 }
--- DataAccess.Model/RoundConfig.cs
+++ DataAccess.Model/RoundConfig.cs
@@ -3,7 +3,6 @@ namespace DataAccess.Model
     public class RoundConfig : IEntityBase
     {
         public long Id { get; set; }
-        //public int RoundNumber { get; set; }
         public long JoiningDuration { get; set; }
         public long RunningDuration { get; set; }
         public long FinishedDuration { get; set; }
