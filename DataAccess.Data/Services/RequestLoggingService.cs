83e04f438bfbeff8c5937f0284407b1889f34b46
shraddhesh singh <shraddhesh.singh@gmail.com>
2020-09-23 23:15:25 +0530

    added requestloggingservice

--- DataAccess.Data/Services/RequestLoggingService.cs
+++ DataAccess.Data/Services/RequestLoggingService.cs
@@ -1,8 +1,11 @@
 ﻿using DataAccess.Data.Abstract;
 using DataAccess.Model;
 using DataAccess.Model.SharedModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,13 +14,31 @@ namespace DataAccess.Data.Services
     public class RequestLoggingService : IRequestLoggingService
     {
         private readonly RequestContext requestContext;
-        public RequestLoggingService(RequestContext requestContext)
+        private readonly DataContext context;
+
+        public RequestLoggingService(RequestContext requestContext, DataContext context)
         {
             this.requestContext = requestContext ?? throw new ArgumentNullException("RequestContext");
+            this.context = context ?? throw new ArgumentNullException("Context");
         }
-        public Task RecordRequest(RequestMethod method, int? statusCode, RequestApi requestApi)
+        public async Task RecordRequest(RequestMethod method, int? statusCode, RequestApi requestApi)
         {
-            throw new NotImplementedException();
+            var currentPhase = await context.Phases.OrderByDescending(p => p.TimeStamp).FirstOrDefaultAsync();
+
+            var newRequest = new Request
+            {
+                GameId = currentPhase?.GameId,
+                RoundId = currentPhase?.RoundId,
+                RequestId = requestContext.RequestId,
+                StatusCode = (HttpStatusCode)statusCode,
+                RequestApi = requestApi,
+                RequestMethod = method,
+                TimeStamp = requestContext.TimeStamp
+            };
+
+            await context.Requests.AddAsync(newRequest);
+
+            await context.SaveChangesAsync();
         }
     }
 }
