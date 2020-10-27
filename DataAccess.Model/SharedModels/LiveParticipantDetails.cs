196bf80f1e121ccd3bae68b6e94565ffe9848d60
ayu004 <34073010+ayu004@users.noreply.github.com>
2020-10-28 02:39:53 +0530

    Update LiveParticipantDetails.cs

--- DataAccess.Model/SharedModels/LiveParticipantDetails.cs
+++ DataAccess.Model/SharedModels/LiveParticipantDetails.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,13 +6,23 @@ namespace DataAccess.Model.SharedModels
 {
     public class LiveParticipantDetails
     {
+
+        public LiveParticipantDetails()
+        {
+            GameId = null;
+            RoundId = null;
+            RoundNumber = null;
+            Phase = null;
+            Lifelines = null;
+            IsAlive = null;
+        }
+
         public string TeamId { get; set; }
-        public long CurrentScore { get; set; }
-        public long TotalScore { get; set; }
+        public long Score { get; set; }
         public bool? IsAlive { get; set; }
         public bool IsRobot { get; set; }
         public string Location { get; set; }
-        public int Lifelines { get; set; }
+        public int? Lifelines { get; set; }
 
         public Guid? GameId { get; set; }
         public Guid? RoundId { get; set; }
